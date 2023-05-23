596b269d2fe16a27e9d0d42b13bc17d0df0eb263
LilaQ <teh.lilaq@gmail.com>
2023-05-23 15:43:58 +0200

    Initial Commit

---
+++ sportnews/sportnewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  sportnewsApp.swift
+//  sportnews
+//
+//  Created by Jan Sallads on 23.05.23.
+//
+
+import SwiftUI
+
+@main
+struct sportnewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
